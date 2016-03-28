7433623fb96b5a97b4b575373cf7f08b72ad9a06
shailly2 <shailly2@ccs.neu.edu>
2016-03-28 15:32:48 -0700

    Reformatted

--- project/pyscripts/pandaspipeline.py
+++ project/pyscripts/pandaspipeline.py
@@ -48,35 +48,32 @@ data = pd.read_csv('C:\Users\shail_000\Documents\GitHub\cs6220project\project\da
 
 cleaned = data['body'].apply(lambda x: re.sub(r'[^a-zA-Z0-9 ]|\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', str(x)).lower())
 result = add_new_column(data, cleaned, 'cleantext')
-#print(result[10:16])
+# print(result[10:16])
 
 tokenizer = RegexpTokenizer(r'\w+')
 tokenized = result['cleantext'].map(lambda x: tokenizer.tokenize(str(x)))
 result = add_new_column(result, tokenized, 'tokens')
-#print(result[10:16])
+# print(result[10:16])
 
 en_stopper = get_stop_words('en')
 stopped = result['tokens'].map(lambda x: stop_token_list(x, en_stopper))
 result = add_new_column(result, stopped, 'stopped')
-#print(result[:16])
+# print(result[:16])
 
 p_stemmer = PorterStemmer()
 stemmed = result['stopped'].map(lambda x: stem_word_list(x))
 result = add_new_column(result, stemmed, 'stemmed')
-#result['stemmed'].to_csv('outputstemmed.txt', ',')
-#print(result[:16])
+# print(result[:16])
 
+columns_to_keep = ['uid', 'stemmed']
+result[columns_to_keep].to_csv('outputstemmed.txt', ',')
 
 word_count = result['stemmed'].map(lambda x: len(x))
 result = add_new_column(result, word_count, 'word_count')
-#print(result[:16])
-
-print "Total number of words", result['word_count'].sum()
+# print(result[:16])
 
 dictionary = corpora.Dictionary(result['stemmed'])
 vocab_count = len(dictionary)
-print "no. of words in Vocabulary", vocab_count
-
 
 corpus = [dictionary.doc2bow(flatten(result['stemmed']))]
 
@@ -85,17 +82,18 @@ top_word_dict = {}
 for top_word in top_20_words:
     top_word_dict[dictionary.get(top_word[0])] = top_word[1]
 
-print top_word_dict
-
-print "Average words in a tweet:", result['word_count'].mean()
-print "No of words  in Longest tweet:", result['word_count'].max()
-print "No of words in shortest tweet:", 1 if result['word_count'].min() == 0 else result['word_count'].min() #some tweets only contain a link
-
 
 gb = result.groupby(["uid"]).size().reset_index(name='count')
 top_20_users = gb.sort(['count'], ascending=False)[:20]
-print top_20_users
 
+print "Total number of words", result['word_count'].sum()
+print "No. of words in Vocabulary", vocab_count
+print "Average words in a tweet:", result['word_count'].mean()
+print "No of words  in Longest tweet:", result['word_count'].max()
+min_words = result['word_count'].min()
+print "No of words in shortest tweet:", 1 if  min_words == 0 else min_words #some tweets only contain a link
+print "Top 20 words and their counts:", top_word_dict
+print "Top 20 users and their tweet counts", top_20_users
 
 # ldamodel = models.ldamodel.LdaModel(corpus, num_topics=3, id2word = dictionary, passes=20)
 #
